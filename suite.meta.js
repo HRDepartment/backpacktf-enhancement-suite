4b44839ae1173536d68fa5fa9983d327cd92f8e2
caresx <tanxc.cares@gmail.com>
2015-05-05 15:53:59 +0200

    download & update urls

--- suite.meta.js
+++ suite.meta.js
@@ -11,6 +11,8 @@
 // @require      https://cdn.rawgit.com/caresx/steam-econcc/2551ce827114e8fd11e94feaa9681bf4aa302379/econcc.js
 // @require      https://cdn.rawgit.com/visionmedia/page.js/1102353025a984f7c88f38538bd62ff89e1eeee6/page.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js
+// @downloadURL  http://caresx.github.io/backpacktf-enhancement-suite/suite.user.js
+// @updateURL    http://caresx.github.io/backpacktf-enhancement-suite/suite.meta.js
 // @run-at       document-end
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
