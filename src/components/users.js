7246ed758dd18484bf35068f05fcb4421627c40d
caresx <tanxc.cares@gmail.com>
2015-09-19 01:05:26 +0200

    New supporter

--- src/components/users.js
+++ src/components/users.js
@@ -14,8 +14,13 @@ var BadgeSupporter = {
     icon: 'fa-trophy',
 };
 
-function Starless(item, particle, margins) {
-    var o = {img: ['https://steamcdn-a.akamaihd.net/apps/440/icons/' + item + '.png', '/images/440/particles/' + particle + '_94x94.png'], star: false};
+var badgemap = [BadgeSelfMade, BadgeSupporter];
+
+function iconinf(item, particle, margins) {
+    var o = {
+        img: 'url(https://steamcdn-a.akamaihd.net/apps/440/icons/' + item + '.png),url(/images/440/particles/' + particle + '_94x94.png)'
+    };
+
     if (margins) {
         if (typeof margins === 'number') o.lmargin = o.rmargin = margins;
         else {
@@ -30,18 +35,21 @@ function Starless(item, particle, margins) {
 }
 
 var users = {
-    "76561198070299574": {badges: [BadgeSelfMade], color: '#028482'},
-    "76561198039453751": {badges: [BadgeSupporter], icon: Starless('soldier_hat.61b68df2672217c4d2a2c98e3ed5e386a389d5cf', 14, [-4, -4])},
-    "76561198068022595": {badges: [BadgeSupporter], color: '#f9d200'},
-    "76561198107654171": {badges: [BadgeSupporter], color: '#0b1c37', icon: Starless('xms2013_demo_plaid_hat.152c6db9806406bd10fd82bd518de3c89ccb6fad', 58, [-7, -8])},
-    "76561198067575136": {badges: [BadgeSupporter], icon: Starless('xms_pyro_parka.de5a5f80e74f428204a4f4a7d094612173adbe50', 13, [-9, -12])},
-    "76561198044195191": {badges: [BadgeSupporter], icon: Starless('fez.ee87ed452e089760f1c9019526d22fcde9ec2450', 43, [-2, -3])},
+    "76561198070299574": {badges: [0], color: '#028482'},
+    "76561198039453751": {badges: [1], icon: ['soldier_hat.61b68df2672217c4d2a2c98e3ed5e386a389d5cf', 14, [-4, -4]]},
+    "76561198068022595": {badges: [1], color: '#f9d200'},
+    "76561198107654171": {badges: [1], color: '#0b1c37', icon: ['xms2013_demo_plaid_hat.152c6db9806406bd10fd82bd518de3c89ccb6fad', 58, [-7, -8]]},
+    "76561198067575136": {badges: [1], icon: ['xms_pyro_parka.de5a5f80e74f428204a4f4a7d094612173adbe50', 13, [-9, -12]]},
+    "76561198044195191": {badges: [1], icon: ['fez.ee87ed452e089760f1c9019526d22fcde9ec2450', 43, [-2, -4]]},
+    "76561198056198948": {badges: [1], icon: ['jul13_soldier_fedora.ec4971943386c378e174786b6302d058e4e8627a', 10, [-5, -6]]},
 };
 
 function renderUserBadges(badges) {
     var html = '';
 
-    badges.forEach(function (badge) {
+    badges.forEach(function (n) {
+        var badge = badgemap[n];
+
         html += '<div data-title="' + badge.title + '" data-content="' + badge.content + '"'+
             ' class="user-badge bes-badge' + (badge.class ? ' ' + badge.class : '') + '"' + (badge.style ? ' style="' + badge.style + '"' : "") + '>'+
             '<i class="fa ' + badge.icon + '"></i></div>';
@@ -73,7 +81,7 @@ function modifyBelts(handle) {
             icon, belt, padding, lmargin, rmargin;
 
         if (!u || !u.icon) return;
-        icon = u.icon;
+        icon = iconinf.apply(null, u.icon);
         belt = this.querySelector('.label-belt');
 
         if (!belt) return;
@@ -83,7 +91,7 @@ function modifyBelts(handle) {
         if (icon.lmargin) lmargin = icon.lmargin;
         if (icon.rmargin) rmargin = icon.rmargin;
 
-        belt.innerHTML = '<span style="background-image:' + icon.img.map(function (img) { return 'url(' + img + ')'; }).join(',') + ';background-size:contain;background-repeat:no-repeat;padding: ' + padding + 'px;margin-left:' + lmargin + 'px;margin-right:' + rmargin + 'px;' + (icon.star !== false ? '' : 'color: transparent;') + '">★</span>';
+        belt.innerHTML = '<span style="background-image:' + icon.img + ';background-size:contain;background-repeat:no-repeat;padding: ' + padding + 'px;margin-left:' + lmargin + 'px;margin-right:' + rmargin + 'px;color: transparent;">★</span>';
     });
 }
 
